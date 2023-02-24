fad8c852a084c86bd6cc067486fca54ffffcd620
Kraichikk <108270237+Kraichikk@users.noreply.github.com>
2023-02-24 15:57:34 +0200

    Add project files.

---
+++ Shortest Word/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Shortest_Word
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindShort("hui pizda mochayobana pi"));
+        }
+        public static int FindShort(string s)
+        {
+            int count = 0;
+            int count2 = s.Length;
+
+            foreach (var c in s)
+            {
+                if (c != ' ')
+                {
+                    count++;
+                }
+                else
+                {
+                    if (count < count2)
+                    {
+                        count2 = count;
+                    }
+
+                    count = 0;
+                }
+            }
+            if (count < count2)
+            {
+                count2 = count;
+            }
+            return count2 ;
+        }
+    }
+}
